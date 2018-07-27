a9e2f971e68d761d308d1e84fa2132da691e7c50
Devin Baran <TheResistance@Devins-MacBook-Pro.local>
2018-07-27 06:14:45 -0400

    Random Quote Generator

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+//Variable to store new quote
+var newQuote;
+
+//Database to hold all of the quotes and their sources
 var quotes = [
   {
     quote: "“No man's knowledge here can go beyond his experience”",
@@ -21,14 +25,25 @@ var quotes = [
   }
 ];
 
-function printQuote() {
-  var randomQuote = quotes.length;
+
+//Function to obtain and hold a random object
+function getRandomQuote(array) {
+  var randomQuote = array.length;
   var randomNumber = Math.floor(Math.random() * randomQuote);
   for(i = 0; i < randomQuote; i++) {
-    var newQuote = '<p class="quote">' + quotes[randomNumber].quote + '</p>';
-    newQuote += '<p class="source">' + quotes[randomNumber].author + '</p>';
+    newQuote = '<p class="quote">' + array[randomNumber].quote + '</p>';
+    newQuote += '<p class="source">' + array[randomNumber].author + '</p>';
   }
+  return newQuote;
+}
+
+
+//Function to grab (getRandomQuote function) and print it to the html document
+function printQuote() {
+  getRandomQuote(quotes)
   document.getElementById('quote-box').innerHTML = newQuote;
 };
 
+
+//OnClick event used to run the printQuote function once the button is pressed
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
